ec5ea8eaa1395f9a2dec1b25105d1ede1db79a32
Ashley Marques <marques.ashley@gmail.com>
2020-03-27 08:52:55 -0300

    mobile app done

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,72 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight + 20,
+    paddingHorizontal: 24
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  incident: {
+    marginTop: 48,
+    padding: 24,
+    borderRadius: 8,
+    marginBottom: 16,
+    backgroundColor: '#fff'
+  },
+  incidentProperty: {
+    fontSize: 14,
+    color: '#41414d',
+    marginTop: 24,
+    fontWeight: 'bold'
+  },
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    color: '#737380'
+  },
+
+  contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    marginBottom: 16,
+    backgroundColor: '#fff'
+  },
+
+  heroTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#13131a',
+    lineHeight: 30,
+  },
+  heroDescription:{
+      fontSize: 15,
+      color: '#737380',
+      marginTop: 16
+  },
+  actions: {
+      marginTop: 16,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+  },
+  action: {
+      backgroundColor: '#e02041',
+      borderRadius: 8,
+      height: 50,
+      width: '48%',
+      justifyContent: 'center',
+      alignItems:'center'
+  },
+  actionText: {
+      color: '#fff',
+      fontSize: 15,
+      fontWeight: 'bold'
+  }
+
+
+});
