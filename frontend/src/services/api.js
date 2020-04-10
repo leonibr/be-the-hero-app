2bde32bba754e674af3874fbb69d24b9b1d6aec9
Ashley Marques <marques.ashley@gmail.com>
2020-04-10 17:07:00 -0300

    docker suport

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-
+const baseURL = process.env.BASE_APP_URL ? process.env.BASE_APP_URL :  'http://localhost:3333';
 const api = axios.create(
     {
-        baseURL: 'http://localhost:3333'
+        baseURL
     });
 
 export default api;
